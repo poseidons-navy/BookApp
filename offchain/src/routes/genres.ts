de2addc62813efdc7c3301f2f3792d426b65430a
Roman Njoroge <roman.njoroge@njuguna.com>
2023-10-20 17:58:26 +0300

    feat: Routes for getting and creating genres

---
+++ offchain/src/routes/genres.ts
@@ -0,0 +1,50 @@
+import express from "express";
+let router = express.Router();
+import { ErrorMessages } from "../constants.js";
+import { SucessMessages } from "../constants.js";
+import { getGenres } from "../model/Genres/getGenres.js";
+import MyError from "../myError.js";
+import { createGenre } from "../model/Genres/createGenre.js";
+
+// Route for getting all categories in system
+router.get("/", async (req, res) => {
+    try {
+        let genres = await getGenres();
+        return res.json(genres);
+    } catch(err) {
+        if (err instanceof MyError) {
+            return res.status(400).json({msg: err.message})
+        } else {
+            return res.status(500).json({msg: ErrorMessages['INTERNAL_SERVER_ERROR']})
+        }
+    }
+});
+
+// Route for creating genres
+router.post("/",async (req, res) => {
+    try {
+        let {
+            name
+        } = req.body;
+        console.log(name);
+        await createGenre(name!)
+        return res.status(201).json({msg: SucessMessages['CREATED_GENRE']});
+    } catch(err) {
+        console.log(err);
+        if (err instanceof MyError) {
+            return res.status(400).json({msg: err.message})
+        } else {
+            return res.status(500).json({msg: ErrorMessages['INTERNAL_SERVER_ERROR']})
+        }
+    }
+})
+
+router.get('/test', (req, res) => {
+    res.send("Can Reach Genres Route!");
+})
+
+router.get("*", (req, res) => {
+    res.status(404).json({msg: ErrorMessages['ROUTE_NOT_EXISTS']});
+})
+
+export default router;
