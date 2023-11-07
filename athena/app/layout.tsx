24d77113ffa1b8d11a5b1df449ccb954a2bc9906
porkytheblack <porkytheblack@gmail.com>
2023-11-08 01:07:52 +0300

    feat: setup authentication with github + wallet creation + base dashboard setup

--- athena/app/layout.tsx
+++ athena/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import AuthProvider from '@/components/hoc/auth-provider'
+import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      
+      <body className={inter.className}>
+        <AuthProvider>
+          {children}
+          <Toaster/>
+        </AuthProvider>
+      </body>
     </html>
   )
 }
---
+++ athena/server/wallet.ts
@@ -0,0 +1,28 @@
+"use server"
+import { prisma } from "@/lib/prisma"
+import { encryptPrivateKey } from "@/lib/utils"
+import algosdk from "algosdk"
+import { getServerAuthSession } from "./auth"
+
+export const createAccount = async (encryptionPassword: string) => {
+    const session = await getServerAuthSession()
+    const account = algosdk.generateAccount()   
+
+    const encryptedPrivateKey = encryptPrivateKey(encryptionPassword, account.sk)
+
+
+    const user = await prisma.user.update({
+        where: {
+            id: session?.user.id
+        },
+        data: {
+            walletAddress: account.addr,
+            encryptedPrivateKey,
+            publicKey: account.addr
+        }
+    })
+
+    return user
+
+
+}
