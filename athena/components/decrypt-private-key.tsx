44d8f2c6bc6a5c98a48b8d711ee2f75496a6200f
Roman Njoroge <roman.njoroge@njuguna.com>
2023-11-15 21:41:42 +0300

    updating decrypt private key button to disappear when clicked

--- athena/components/decrypt-private-key.tsx
+++ athena/components/decrypt-private-key.tsx
@@ -5,52 +5,54 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, Di
 import { Input } from "./ui/input";
 import { useState } from "react";
 import { useToast } from "./ui/use-toast";
-
+import { Heart } from "lucide-react";
 
 export default function DecryptPrivateKey(props: { visible: boolean }) {
-    const [password, setPassword] = useState<string>()
-    const { decryptPrivateKey, privateKey } = usePrivateKey()
-    const { toast } = useToast()
+    const [password, setPassword] = useState<string>();
+    const { decryptPrivateKey, privateKey } = usePrivateKey();
+    const { toast } = useToast();
+    const [isVisible, setIsVisible] = useState(props.visible);
 
-    const { visible } = props;
     const handleDecrypt = () => {
         try {
-            password && decryptPrivateKey?.(password)
-
-        }
-        catch (e) {
+            password && decryptPrivateKey?.(password);
+            setIsVisible(false); // Set visibility to false to close the component
+        } catch (e) {
             toast({
                 variant: "destructive",
                 title: "!Uh-oh",
                 description: "Are you sure the password is valid"
-            })
+            });
         }
-    }
+    };
+
     return (
         <div>
-            {visible ? <Dialog>
-                <DialogTrigger>
-                    <Button>
-                        Decrypt Private Key
-                    </Button>
-                </DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>
-                            Decrypt your private key
-                        </DialogTitle>
-                        <DialogDescription>
-                            This operation requires you to sign with your private key, decrypt it to proceed
-                        </DialogDescription>
-                    </DialogHeader>
-                    <div className="flex flex-col w-full gap-y-2 ">
-                        <Input onChange={(e) => setPassword(e.target.value)} placeholder="Your cool password..." type="password" />
-                        <Button variant={'outline'} onClick={handleDecrypt} >
-                            Decrypt
+            {isVisible ? (
+                <Dialog>
+                    <DialogTrigger>
+                        <Button>
+                            Decrypt Private Key to purchase
                         </Button>
-                    </div>
-                </DialogContent>
-            </Dialog> : <></>}
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>
+                                Decrypt your private key
+                            </DialogTitle>
+                            <DialogDescription>
+                                This operation requires you to sign with your private key, decrypt it to proceed
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div className="flex flex-col w-full gap-y-2 ">
+                            <Input onChange={(e) => setPassword(e.target.value)} placeholder="Your cool password..." type="password" />
+                            <Button variant={'outline'} onClick={handleDecrypt} >
+                                Decrypt
+                            </Button>
+                        </div>
+                    </DialogContent>
+                </Dialog>
+            ) : null}
         </div>
-    )
+    );
 }
